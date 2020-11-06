739c32f7f4188860974c861b74a6dc0862d92a44
Max Brockbank <maxbrockbank1999@gmail.com>
2020-11-06 09:47:20 -0800

    pass test 2

--- js/scripts.js
+++ js/scripts.js
@@ -3,19 +3,26 @@
 function Pizza(size, toppings){
   this.size = size;
   this.toppings = toppings;
+  this.price = 0;
 }
 
+function gatherToppingsInput(){
+  let selectedToppings = [];
+  for(let i = 0; i < $("input[name=toppings]").length; i++ ){
+    if($("input[name=toppings]")[i].checked){
+      selectedToppings.push($("input[name=toppings]")[i]);
+    }
+  }
+  return selectedToppings;
+}
 
 $(document).ready(function(){
 
   $("#pizza").submit(function(event){
     event.preventDefault();
-    
     let size = $("input[name=size]:radio:checked");
-    let toppings = $("input[name=toppings]")
-    let price = []
-
-    console.log($("input[name=toppings]:checked").attr("id"));
+    let toppings = gatherToppingsInput();
+    // console.log(toppings[0].defaultValue);
   })
 
 })
